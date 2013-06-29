3cb07affa64f95b8b46e03c30f444f8d71b06092
vardhan-duvvuri <vardhan.duvvuri@ooutlook.com>
2013-06-29 09:24:50 +0530

    finding the indeces,sum of which values equal to given no

---
+++ sumfind.py
@@ -0,0 +1,27 @@
+def sum(a,i,j,k):
+    s=a[i]+a[j]
+    if s == k:
+        print "a["+str(i)+"] + a["+str(j)+"] = "+str(k)
+    elif s > k:
+        j-=1
+        sum(a,i,j,k)
+    else:
+        i+=1
+        sum(a,i,j,k)
+    
+def get_data():
+	nums=[]
+	while True:
+		a=int(raw_input("Enter a number (-1 to exit) : "))
+		if a==-1:
+			break
+		nums.append(a)
+	return nums
+
+if __name__=='__main__':
+	nums=get_data()
+	print "Input data is : ",nums
+	i=0
+	j=len(nums)-1
+	k=int(raw_input("Enter the k value : "))
+	sum(nums,i,j,k)
